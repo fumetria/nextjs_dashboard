9a552b085d1aa4fc0bf4cb4f22ce45350d9a919b
fumetria <fulin789@gmail.com>
2025-05-30 16:28:40 +0200

    Add customer information in customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Table from "@/app/ui/customers/table";
+import Search from '@/app/ui/search';
+import Pagination from "@/app/ui/invoices/pagination";
+import { Suspense } from "react";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchAllCustomers, fetchCustomersPages } from "@/app/lib/data";
+
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParam = await props.searchParams;
+    const query = searchParam?.query || '';
+    const currentPage = Number(searchParam?.page) || 1;
+    const totalPages = await fetchCustomersPages(query);
+    const customers = await fetchAllCustomers();
+    return (
+
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+            </div>
+            <Table customers={customers}>
+            </Table>
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
+    )
+
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -216,3 +216,48 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+export async function fetchCustomersPages(query: string) {
+  try {
+    const data = await sql`SELECT COUNT(*)
+    FROM customers
+  `;
+
+    const totalPages = Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of invoices.');
+  }
+}
+
+export async function fetchAllCustomers() {
+  try {
+    const data = await sql<CustomersTableType[]>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+	  `;
+
+    const customers = data.map((customer) => ({
+      ...customer,
+      total_pending: formatCurrency(customer.total_pending),
+      total_paid: formatCurrency(customer.total_paid),
+    }));
+
+    return customers;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch customer table.');
+  }
+}
+
