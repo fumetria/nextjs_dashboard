c8195a4cb34cd184349b850f3d2a30d5a44c48d8
fumetria <fulin789@gmail.com>
2025-05-30 17:11:53 +0200

    Add search functionality to customers page and remove redundant search from table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -24,6 +24,7 @@ export default async function Page(props: {
                 <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search customer" />
             </div>
             <Table customers={customers}>
             </Table>
